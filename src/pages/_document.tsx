af73feb272f0dd057f694ed65eb815206f243110
ethanshealey <ethan.shealey@gmail.com>
2023-08-12 22:09:16 -0400

    font fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <meta name="description" content="A stock app by Ethan Shealey" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo1.png" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" />
       </Head>
       <body>
         <Main />
